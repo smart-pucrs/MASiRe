bea10929c108849af7af7700b4ff0e21b6c7ce32
henrique.kops <henrique.kops@acad.pucrs.br>
2018-09-12 18:44:22 -0300

    agent and action_executor classes modified

--- simulation/action_executor.py
+++ simulation/action_executor.py
@@ -2,6 +2,206 @@
 
 class ActionExecutor:
 
-  def __init__(self, config):
+ 	def __init__(self, config):
+ 		self.a = 'a'
 
-    self.a = 'a'
+ 	def parse_action():
+ 		#parses the action in a string of commands
+
+ 	def parse_parameters():
+ 		#parses the parameters of an action
+
+ 	def execute(agent, command):
+ 		action = parse_action(command)
+ 		parameters = parse_parameters(command) 
+
+ 		if action is None:
+ 			agent.set_last_action_result(False)
+ 			#log(failed_no_action)
+
+ 		elif action is 'move':
+ 			if parameters.size() < 1 or parameters.size() > 2:
+	 				#log(failed_wrong_param)
+	 		else:		
+	 			try:
+		 			if parameters.size() == 1:
+		 				facility = parameters[0]
+		 				try:
+		 					if(agent.get_route is None):
+		 						try:
+		 							route = create_route_facility(agent.get_location, facility)
+		 							agent.set_route(route)
+		 						except:
+		 							#log(failed_unknown_facility)
+		 					else: 
+		 						agent.set_route(agent.get_route().get_next_node())
+		 				except:
+		 					#log(failed_no_route)
+		 			elif parameters.size() == 2:
+		 				latitude = parameters[0]
+		 				longitude = parameters[1]
+		 				try:
+		 					route.create_route_location(agent.get_location, latitude, longitude)
+		 				except:
+		 					#log(failed_no_route)
+ 				except:
+ 					#log(failed)
+
+
+ 		elif action is 'deliver_physical':
+	 		if parameters.size() < 1 or parameters.size() > 2:
+	 			#log(failed_wrong_param)
+	 		else:
+	 			try:
+		 			if parameters.size() == 1:
+		 				agent_deliver('pyshical', parameters[0])
+		 			elif parameters.size() == 2:
+		 				agent_deliver('physical', parameters[0], parameters[1])
+	 			except:
+	 				#log(failed)
+
+ 		elif action is 'deliver_virtual':
+ 			if parameters.size() < 1 or parameters.size() > 2:
+		 			#log(failed_wrong_param)	
+ 			else:
+				try:
+		 			if parameters.size() == 1:
+		 				agent_deliver('virtual', parameters[0])
+		 			elif parameters.size() == 2:
+		 				agent_deliver('virtual', parameters[0], parameters[1])
+		 		except:
+	 				#log(failed)
+
+
+ 		elif action is 'charge':
+ 			if parameters.size() > 0:
+ 				#log(failed_wrong_param)
+ 			else:
+ 				try:
+ 					if check_cdm_location(agent.get_location): #not implemented yet
+ 						agent.charge()
+ 					else:
+ 						#log(failed_location)
+ 				except:
+ 					#log(failed)
+
+
+ 		elif action is 'rescue_victim':
+ 			if parameters.size() > 1:
+ 				#log(failed_wrong_param)
+ 			else:
+ 				try:
+					victim = search_victim(parameters[0]) #not implemented yet
+ 					if victim is None:
+ 						#log(failed_unknown_item)
+ 					try:
+ 						victim.rescue_victim()
+ 					except:
+ 						#log(failed_location)
+ 					weight = victim.get_weight()
+ 					if agent.get_free_physical_capacity() > victim.get_weight():
+ 						agent.add_physical_item(victim.get_name(), weight)
+ 					else:
+ 						#log(failed_capacity)
+ 				except:
+ 					#log(failed)	
+
+
+ 		elif action is 'collect_water':
+		 	if parameters.size() > 0:
+		 		#log(failed_wrong_param)
+		 	else:
+		 		try:
+		 			if agent.get_free_physical_capacity() > 0: #?
+		 				try:
+		 					water.collect(agent) #not implemented yet
+		 				except:
+		 					#log(failed_location)
+		 			else:
+		 				#log(failed_capacity)
+		 		except:
+		 			#log(failed)
+
+
+ 		elif action is 'photograph':
+ 			if parameters.size() > 0:
+ 				#log(failed_wrong_param)
+ 			else:
+ 				try:
+ 					if agent.get_free_virtual_capacity() > 0: #?
+ 						try:
+ 							role.get_photograph(agent.get_location()) #not implemented yet
+ 							agent.add_virtual_item('photo') # photo_1, 2, 3 ... (?) - amount?
+ 						except:
+ 							#log(failed_location)
+ 					else:
+ 						#log(failed_capacity)
+ 				except:
+ 					#log(failed)
+
+
+ 		elif action is 'search_social_asset':
+ 			if parameters.size() != 1 or parameters.size != 3:
+ 				#log(failed_wrong_param)
+ 			else:
+ 				try:
+ 					if parameters.size() == 1:
+ 						_map.search_social_asset(radius) #not implemented yet - agent location(?)
+ 					else:
+ 						_map.search_social_asset(radius, latitude, longitude) #not implemented yet - agent location(?)
+ 				except:
+ 					#log(failed)
+
+
+ 		elif action is 'analyze_photo':
+ 			if parameters.size() > 0:
+ 				#log(failed_wrong_param)
+ 			else:
+ 				try:
+ 					if agent.get_free_virtual_capacity() == agent.get_initial_virtual_capacity():
+ 						success = world.analyze() #not implemented yet (world?)
+ 						if success:
+ 							agent.remove_virtual_item('photo') #photo_1, 2,3 ... (?) - amount?
+ 					else:
+ 						#log(failed_item_amount)
+ 				except:
+ 					#log(failed)
+
+
+ 		else:
+ 			#log(failed)
+
+
+ 	def agent_deliver(self, kind, item, amount = None):
+	 	if amount is None:
+	 		try:
+	 			if kind is 'physical':
+	 				agent.remove_physical_item(item)
+	 			elif kind is 'virtual':
+	 				agent.remove_virtual_item(item)
+	 			else:
+	 				#log(failed_invalid_kind)
+	 				raise Exception('invalid kind for delivery')
+	 		except:
+	 			#log(failed_unknown_item)
+	 		try:
+	 			cdm.deliver(item) #not implemented yet
+	 		except:
+	 			#log(failed_location)
+	 	elif amount is not None:
+	 		try:
+	 			verify(amount)#not implemented yet
+	 		except:
+	 			#log(failed_item_amount)
+	 		try:
+	 			if kind is 'physical':
+	 				agent.remove_physical_item(name)
+	 			elif kind is 'virtual':
+	 				agent.remove_virtual_item(name)
+	 		except:
+	 			#log(failed_unknown_item)
+	 		try:
+	 			cdm.deliver(item, amount) #not implemented yet
+	 		except:
+	 			#log(failed_location)
+
--- simulation/data/agent.py
+++ simulation/data/agent.py
@@ -1,1 +1,98 @@
 # based on https://github.com/agentcontest/massim/blob/master/server/src/main/java/massim/scenario/city/data/Entity.java
+
+class Agent:
+
+	last_action = 'None'
+	last_action_result = True
+	location = [0,0]
+	route = None
+	physical_storage = []
+	virtual_storage = []
+
+#constructor with agent's private attributes
+	def __init__ (self, identifier, kind):
+		self.identifier = identifier 
+		self.kind = kind
+
+#getters
+	def get_identifier():
+		return self.identifier
+	def get_kind():
+		return self.kind
+	def get_skills():
+		return self.skills
+	def get_battery():
+		return self.battery
+	def get_speed():
+		return self.speed
+	def get_location():
+		return self.location
+	def get_free_physical_capacity():
+		return self.physical_capacity
+	def get_free_virtual_capacity():
+		return self.virtual_capacity
+	def get_initial_physical_capacity():
+		return self.initial_physical_capacity
+	def get_initial_virtual_capacity():
+		return self.initial_virtual_capacity
+	def get_last_action():
+		return last_action
+	def get_last_action_result():
+		return self.last_action_result
+	def get_route():
+		return self.route
+	def remove_physical_item(item):
+		#self.physical_storage += amount (?)
+		self.physical_storage.remove(item)
+		return #(?)
+	def remove_virtual_item(item):
+		#self.virtual_storage += amount (?)
+		self.virtual_storage.remove(item)
+		return #(?)
+#setters
+	def set_skills(skills):
+		self.skills = skills
+	
+	def discharge():
+		self.battery = 0
+
+	def charge(amount):
+		self.battery = total_battery
+
+	def set_battery(amount):
+		self.total_battery = amount
+
+	def set_speed(speed):
+		self.speed = speed
+
+	def set_location(x, y):
+		location[0] = x
+		location[1] = y
+
+	def set_free_physical_capacity(space):
+		self.initial_physical_capacity = space
+		self.physical_capacity = space
+
+	def set_free_virtual_capacity(space):
+		self.initial_virtual_capacity = space
+		self.virtual_capacity = space
+
+	def set_last_action(action):
+		self.last_action = action
+
+	def set_last_action_result(value):
+		self.last_action_result = value
+
+	def set_route(Route route):
+		self.route = route
+
+	def add_physical_item(item, amount=None):
+		self.physical_storage.append(item)
+		if amount is not None:
+			self.physical_capacity -= amount
+
+	def add_virtual_item(item, amount=None):
+		self.virtual_storage.append(item)
+		if amount is not None:
+			self.virtual_capacity -= amount
+
