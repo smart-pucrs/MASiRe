e2f07ba2a9c52a395c06edda5e6864a706efacd7
Unknown <joaovbsevero@gmail.com>
2018-09-28 20:36:27 -0300

    Created functions to each emit

--- src/communication/events/emiters.py
+++ src/communication/events/emiters.py
@@ -1,8 +1,16 @@
 from flask_socketio import emit
 
 
-def response_to_action(response, token):
+def response_to_action_ready(response, token):
     emit(token, response, callback=ack)
+
+
+def response_to_action_deliver(event, response):
+    emit(event, response)
+
+
+def response_to_action_connect(event, response):
+    emit(event, response)
 
 
 def ack():
