e270d919dfb4b44c27f3a5d8dc0c8c8d14391f2e
Unknown <joaovbsevero@gmail.com>
2018-10-05 18:33:12 -0300

    Set a timeout to the first connection

--- src/communication/events/listeners.py
+++ src/communication/events/listeners.py
@@ -1,4 +1,3 @@
-import _thread
 import time
 import jwt
 import json
@@ -7,39 +6,49 @@ from src.communication.ActionResult import ActionResult
 from src.__init__ import socketio
 from src.communication.events.emiters import response_to_action_connect, response_to_action_deliver, \
     response_to_action_ready
-from src.communication.events.prepare_action import verify_method
+from src.communication.events.prepare_action import handle_request
 from src.communication.AgentManager import AgentManager
 
 agent_manager = AgentManager()
-valid_time = True
-init = time.time()
+init_general = None
+
+agents = []
 
 
 @socketio.on('receive_jobs')
 def handle_connection(message):
+    global init_general
+
     agent = json.loads(message)
-
     agent = (agent['id'], (agent['method'], agent['parameters'][0], agent['parameters'][1]))
-    method = agent[1][0]
 
-    verified = verify_method(method, agent)
+    verified = handle_request(agent)
+
     response = ('received_jobs_result', verified)
     call_responses(response, 'receive_jobs')
 
 
 @socketio.on('connect')
 def respond_to_request(message=None):
-    global init, valid_time
+    global init_general, agents
 
-    init = time.time()
-    _thread.start_new_thread(start_timer, (None,))
-    response = ('connection_result', {'success': True})
+    if init_general is None:
+        init_general = time.time()
 
-    call_responses(response, 'connect')
+    if time.time() - init_general < 320:
+        response = ['connection_result', '']
+        if len(agents) <= 5:
+            response[1] = 'Success'
+            response = (response[0], response[1])
+            call_responses(response, 'connect')
 
-    if not valid_time:
-        # socketio.
-        pass
+        else:
+            response[1] = 'Failure'
+            response = (response[0], response[1])
+            call_responses(response, 'connect')
+
+    else:
+        call_responses(['connection_result', 'Time is up! Sorry'], 'connect')
 
 
 @socketio.on('ready')
@@ -62,12 +71,3 @@ def call_responses(results, caller):
 
     elif caller == 'receive_jobs':
         response_to_action_deliver(results[0], results[1])
-
-
-def start_timer(input=None):
-    global valid_time
-
-    while 1:
-        if time.time() - init > 3:
-            valid_time = False
-            break
