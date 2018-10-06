1e41a218d6367db8b6863e5dda0c9b9a5a71ad3d
Unknown <joaovbsevero@gmail.com>
2018-10-05 22:26:00 -0300

    Added count to match the expected from the simulation

--- src/communication/events/listeners.py
+++ src/communication/events/listeners.py
@@ -2,31 +2,36 @@ import time
 import jwt
 import json
 
-from src.communication.action_result import ActionResult
 from src.__init__ import socketio
 from src.communication.events.emiters import response_to_action_connect, response_to_action_deliver, \
     response_to_action_ready, emit_pre_step
-from src.communication.events.prepare_action import handle_request
 from src.communication.agent_manager import AgentManager
+from src.communication.events.prepare_action import handle_request
+from src.manager import simulation_instance
 
 agent_manager = AgentManager()
 init_general = None
 
 agents = []
+count = 1
 
 
 @socketio.on('receive_jobs')
 def handle_connection(message):
+    global count
 
+    message = json.loads(message)
     agent = (message['id'], (message['method'], message['parameters'][0], message['parameters'][1]))
     method = agent[1][0]
+    simulation_manager = simulation_instance.get_instance('')
 
-    from src.manager import SimulationInstance
-    simulation_manager = SimulationInstance.get_instance('')
-    response = simulation_manager.do_step([agent])
-    response = ('received_jobs_result', response)
-    call_responses(response, 'receive_jobs','')
+    if handle_request(agent):
+        agent = (count, (agent[1][0], agent[1][1], agent[1][2]))
+        count += 1
+        response = simulation_manager.do_step([agent])
+        response = ('received_jobs_result', response)
 
+        call_responses(response, 'receive_jobs', '')
 
 
 @socketio.on('connect')
@@ -46,7 +51,7 @@ def respond_to_request(message=None):
         if len(agents) <= 5:
             response[1] = 'Success'
             response = (response[0], response[1])
-            call_responses(response, 'connect','')
+            call_responses(response, 'connect', '')
 
         else:
             response[1] = 'Failure'
@@ -62,8 +67,8 @@ def respond_to_request_ready(message):
     decoded = jwt.decode(message['data'], 'secret', algorithms=['HS256'])
     print('ready  :', decoded)
     agent_manager.manage_agents(decoded)
-    from src.manager import SimulationInstance
-    simulation_manager = SimulationInstance.get_instance('')
+    from src.manager import simulation_instance
+    simulation_manager = simulation_instance.get_instance('')
     call_responses(simulation_manager.agents_list(), 'ready', message['data'])
     response = simulation_manager.do_pre_step()
     emit_pre_step(response, message['data'])
