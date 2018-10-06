0a8602cbcdef1eb6ca840afedb916e899b5ccc82
Unknown <joaovbsevero@gmail.com>
2018-10-05 22:25:30 -0300

    Little fix

--- start_simulation.py
+++ start_simulation.py
@@ -12,5 +12,5 @@ from src.manager import simulation_instance
 # Roda o servidor flask-socketIO
 project_path = os.path.dirname(os.path.realpath(__file__))
 run([str(project_path) + "/venv/Scripts/python", "-m", "flask", "run"],
-    env=dict(FLASK_APP='src/app.py', FLASK_ENV='development', FLASK_DEBUG='0', **os.environ))
+    env=dict(FLASK_APP='src/app.py', FLASK_ENV='development', FLASK_DEBUG='1', **os.environ))
 
