f6d1a47260af3441bba04811b51a535531a622ad
Max Franke <max.franke@acad.pucrs.br>
2018-10-25 03:12:53 -0300

    60% done.

--- src/simulation/generator.py
+++ src/simulation/generator.py
@@ -2,11 +2,12 @@
 # /Generator.java
 
 import random
-
 from src.simulation.data.events.flood import Flood
 from src.simulation.data.events.photo import Photo
 from src.simulation.data.events.victim import Victim
 from src.simulation.data.events.water_sample import WaterSample
+from pyroutelib3 import Router  # Import the router
+import math
 
 
 class Generator:
@@ -15,11 +16,11 @@ class Generator:
 
         self.config = config
         random.seed(config['map']['randomSeed'])
+        self.router = Router("car", config['map']['map'])  # Initialise router object from pyroutelib3
 
     def generate_events(self):
 
         events = [None for x in range(self.config['map']['steps'])]
-
         for step in range(len(events)):
 
             # generate floods (index 0) and photo events (index 1)
@@ -61,6 +62,10 @@ class Generator:
                                self.config['generate']['flood']['rectangle']['maxLength'])
             )
 
+        flood_lat = random.uniform(self.config['map']['minLat'], self.config['map']['maxLat'])
+        flood_lon = random.uniform(self.config['map']['minLon'], self.config['map']['maxLon'])
+        dimensions['coord'] = flood_lat, flood_lon
+
         photos = self.generate_photos()
         water_samples = self.generate_water_samples()
 
@@ -93,7 +98,7 @@ class Generator:
         ))]
 
         for y in range(len(photo_victims)):
-            
+
             victim_size = random.randint(
                 self.config['generate']['victim']['minSize'],
                 self.config['generate']['victim']['maxSize']
@@ -119,3 +124,20 @@ class Generator:
             water_samples[x] = WaterSample(self.config['generate']['waterSample']['size'])
 
         return water_samples
+
+    def nodes_in_radius(self, coord, radius):
+        # radius in kilometers
+        result = []
+        for node in self.router.rnodes:
+            if self.router.distance(self.node_to_radian(node), self.coords_to_radian(coord)) <= radius:
+                result.append(node)
+        return result
+
+    def node_to_radian(self, node):
+        """Returns the radian coordinates of a given OSM node"""
+        return self.coords_to_radian(self.router.nodeLatLon(node))
+
+    def coords_to_radian(self, coords):
+        """Maps a coordinate from degrees to radians"""
+        return list(map(math.radians, coords))
+
