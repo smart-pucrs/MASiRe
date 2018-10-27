b5b1d48277010f181dd3146c91f2518ba78245bd
dudupqv <eduardo.ferreira.002@acad.pucrs.br>
2018-10-27 10:48:48 -0300

    comments and improvements

--- src/communication/events/controller.py
+++ src/communication/events/controller.py
@@ -7,16 +7,18 @@ class Controller:
         self.timer = time.time()
         self.init_general = None
 
+    #initializes the timer if it has not been initialized
+    #check if the total agents exceeded
     def check_population(self):
         if self.init_general is None:
             self.init_general = time.time()
 
-        if self.check_timeraux():
+        if self.check_timer():
             if len(self.agents) > 5:
                 return True
             else:
                 return False
-
+    #check if connection period for new agents is open
     def check_timer(self):
         if time.time() - self.init_general < 3600:
             return True
