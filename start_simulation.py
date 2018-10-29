1b6fdf0c137bfba948ae2d1297edea2b2548703b
jojo0severo <joaovbsevero@gmail.com>
2018-10-29 11:04:13 -0300

    Added pip files to manage pipenv

--- start_simulation.py
+++ start_simulation.py
@@ -9,8 +9,13 @@ from src.manager import simulation_instance
 simulation_instance.get_instance('config.json')
 
 
-# Roda o servidor flask-socketIO
-project_path = os.path.dirname(os.path.realpath(__file__))
-run([str(project_path) + "/venv/Scripts/python", "-m", "flask", "run", "--host=0.0.0.0"],
-    env=dict(FLASK_APP='src/app.py', FLASK_ENV='development', FLASK_DEBUG='1', **os.environ))
+'''
+    O complemento APOS o USERPROFILE eh o caminho para o virtualenv da maquina que o criou, caso haja problemas
+    O caminho até o .virtualenvs eh padrao para todas as maquinas, verificar o nome da pasta contendo 'desastres'
+    no nome e substituir o nome visto pelo nome 'desastres-8vkced-v'
+'''
 
+# Roda o servidor flask-socketIO
+project_path = os.environ['USERPROFILE'] + "/.virtualenvs/desastres-8vkced-v/scripts/python"
+run([str(project_path), "-m", "flask", "run", "--host=0.0.0.0"],
+    env=dict(FLASK_APP='src/app.py', FLASK_ENV='development', FLASK_DEBUG='1', **os.environ))
