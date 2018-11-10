b720caa2e893da743e9085fe3141d933cf288b9c
henrique.kops <henrique.kops@acad.pucrs.br>
2018-11-09 22:00:48 -0200

    Comments. Not yet complete.

--- src/simulation/data/cdm.py
+++ src/simulation/data/cdm.py
@@ -4,9 +4,15 @@
 class Cdm:
 
     def __init__(self, location):
+        """
+        [Object that represents the simulation CDM,
+        which will receive physical an virtual items from
+        the active agents.]
+
+        :param location: The location of the CDM at the simulation map.
+        This variable will restrict the agent's actions in case of location inequality.
+        """
         self.storedVolume = 0
         self.virtualItems = []
         self.physicalItems = []
         self.location = location  
-
-    #def add_virtual(self, item, )
--- src/simulation/exceptions.py
+++ src/simulation/exceptions.py
@@ -1,39 +1,50 @@
+"""
+[All the exceptions constructors which are initiated by a desired error message.]
+"""
+
 class Failed_wrong_param(Exception):
 
-    def __init__(self,message=None):
+    def __init__(self, message=None):
         self.message = message
+
 
 class Failed_unknown_facility(Exception):
 
-    def __init__(self,message=None):
+    def __init__(self, message=None):
         self.message = message
+
 
 class Failed_no_route(Exception):
 
-    def __init__(self,message=None):
+    def __init__(self, message=None):
         self.message = message
+
 
 class Failed_capacity(Exception):
 
-    def __init__(self,message):
+    def __init__(self, message):
         self.message = message
+
 
 class Failed_location(Exception):
 
-    def __init__(self,message):
+    def __init__(self, message):
         self.message = message
+
 
 class Failed_unknown_item(Exception):
 
-    def __init__(self,message):
+    def __init__(self, message):
         self.message = message
+
 
 class Failed_item_amount(Exception):
 
-    def __init__(self,message):
+    def __init__(self, message):
         self.message = message
+
 
 class Failed_invalid_kind(Exception):
 
-    def __init__(self,message):
+    def __init__(self, message):
         self.message = message
