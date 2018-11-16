fde960e99966654f8f4875117332f58161fbd91d
gpaulbr <gpaulbr@gmail.com>
2018-11-16 21:54:53 -0200

    updated configuration comment

--- start_simulation.py
+++ start_simulation.py
@@ -8,15 +8,11 @@ from src.manager import simulation_instance
 # Define the instance of the simulation with its startup file
 simulation_instance.get_instance('config.json')
 
-
-'''
-    The path after USERPROFILE depends on each computer
-    To properly use the simulation configure the path to the correct for your computer 
-'''
-
 # Run the flask socketIO server
 
 project_path = os.getcwd()\
+
+# Here are setted the directory at venv where the python executable are
 
 if os.name == 'nt':
     project_path += '\\venv\\Scripts\\python.exe'
