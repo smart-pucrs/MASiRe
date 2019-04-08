dfb0b7be1444b77b941ce7e9aae5e79dfa4e5976
jojo <joaovbsevero@gmail.com>
2019-04-08 19:12:14 -0300

    Refactoring module to improve readability

--- start_simulation.py
+++ start_simulation.py
@@ -1,25 +1,30 @@
-import sys
-from subprocess import run
 import os
-
-from src.manager import simulation_instance
+import pathlib
+from subprocess import run
+from src.manager.simulation_instance import SimulationSingleton
 
 
 # Define the instance of the simulation with its startup file
 def start_instance():
     SimulationSingleton('files/config.json').simulation_manager
 
-# Run the flask socketIO server
-
-project_path = os.getcwd()
 
 # Here are setted the directory at venv where the python executable are
+def start_simulation():
+    interpreter_path = str(pathlib.Path(__file__).parent.absolute())
 
-if os.name == 'nt':
-    project_path += '\\venv\\Scripts\\python.exe'
-else:
-    project_path += "/venv/bin/python"
+    # Windows based OS
+    if os.name == 'nt':
+        interpreter_path += '\\venv\\Scripts\\python.exe'
 
-run([str(project_path), "-m", "flask", "run", "--host=0.0.0.0"],
-    env=dict(FLASK_APP='src/app.py', FLASK_ENV='development', FLASK_DEBUG='1', **os.environ))
+    # Linux based OS
+    else:
+        interpreter_path += "/venv/bin/python"
 
+    run([str(interpreter_path), "-m", "flask", "run", "--host=0.0.0.0"],
+        env=dict(FLASK_APP='src/app.py', FLASK_ENV='development', FLASK_DEBUG='1', **os.environ))
+
+
+if __name__ == '__main__':
+    start_instance()
+    start_simulation()
