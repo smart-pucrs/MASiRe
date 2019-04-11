ea9ab0e0c1a4ba24084796ba64bce07cf07a0d1f
jojo <joaovbsevero@gmail.com>
2019-04-11 19:06:53 -0300

    Removing old client and renaming newer
    To avoid confusion the newer client was named as client and the old one was removed

--- agents/client.py
+++ agents/client.py
@@ -1,32 +1,38 @@
 from socketIO_client import SocketIO, LoggingNamespace
 import json
 
-
 # ==================== ATENTION, ALL THE LISTEN EVENTS WERE CHANGED ===========================
 
 
 agent = {
-    "name": "Toyota",
+    "name": "ubuntu",
     "version": "1.3",
-    "type": "Car",
     "owner": "87462"
 }
 
 token = None
 response_ready = False
+response_connect = False
+response_job = False
+
+
+def on_response_job_result(args):
+    print('Respondeu pra mim')
+    print(args)
+
+
+def on_response_jobs(args):
+    print('Response: ', json.loads(args))
 
 
 def on_response_connection(args):
     global response_connect
 
-    if 'token' in args:
-        socketIO.emit('receive_jobs', json.dumps(step_config_agent))
-
     response_connect = True
     print('Response: ', json.loads(args))
 
 
-def on_response_ready(args):
+def on_response_received(args):
     global token, response_ready
 
     response_ready = True
@@ -36,9 +42,9 @@ def on_response_ready(args):
     print('Response: ', args[0])
 
 
-socketIO = SocketIO('localhost', 5000, LoggingNamespace)
-socketIO.on('ready', on_response_ready)
-socketIO.emit('first_message', json.dumps(agent))
+socketIO = SocketIO('localhost', 12345, LoggingNamespace)
+socketIO.on('received', on_response_received)
+socketIO.emit('time_ended')
 
 socketIO.wait(2)
 
@@ -60,8 +66,15 @@ step_config_agent = {
     'parameters': ['24', '32']
 }
 
+event = 'job_result'
+socketIO.on(event, on_response_job_result)
+socketIO.on('received_jobs_result', on_response_jobs)
 socketIO.emit('receive_jobs', json.dumps(step_config_agent))
 
 socketIO.wait(60)
 
-print(token)
+print(str(token))
+
+
+
+
