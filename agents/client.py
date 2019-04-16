85f482a94a7f683c95669d459e5599986f336b3b
jojo <joaovbsevero@gmail.com>
2019-04-16 17:46:54 -0300

    Testing agent
    The modified agent to run against the simulation, it is using http, the new way to communicate with the simulation

--- agents/client.py
+++ agents/client.py
@@ -1,21 +1,48 @@
 import requests
+import multiprocessing
+from flask import Flask, request
 
+
+app = Flask(__name__)
 
 agent = {
     "name": "ubuntu",
-    "version": "1.3",
-    "owner": "87462"
+    "url": "http://localhost:45678/ubuntu"
+}
+
+action = {
+    "token": agent['token'],
+    "action": "move",
+    "parameters": [-30.109611, -51.211556]
 }
 
 
-agent['url'] = agent['url'] + agent['name']
+@app.route(agent['url'])
+def listen():
+    results = request.get_json()
+    print(results)
+
+
+def start_app():
+    app.run(port=45678)
 
 
 if __name__ == '__main__':
+    aux = multiprocessing.Process(target=start_app)
+    aux.start()
+
     response = requests.post('http://localhost:12345/connect_agent', json=agent).json()
+    print(response)
     agent['token'] = response['data']
+
     response = requests.post('http://localhost:12345/validate_agent', json=agent['token']).json()
     print(response)
+    agent = response
+
+    response = requests.post('http://localhost:12345/job', json=action).json()
+    print(response)
+
+    aux.join()
 
 
 
