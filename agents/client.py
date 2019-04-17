0a84d25da63aec1dd58dc2c134b5b33bce0f6579
jojo <joaovbsevero@gmail.com>
2019-04-17 17:04:13 -0300

    Adapted the agent
    The agent was adapted to match the current state of the simulation

--- agents/client.py
+++ agents/client.py
@@ -1,39 +1,20 @@
 import requests
-import multiprocessing
-from flask import Flask, request
-
-
-app = Flask(__name__)
 
 agent = {
     "name": "ubuntu",
-    "url": "http://localhost:45678/ubuntu"
 }
-
-action = {
-    "token": agent['token'],
-    "action": "move",
-    "parameters": [-30.109611, -51.211556]
-}
-
-
-@app.route(agent['url'])
-def listen():
-    results = request.get_json()
-    print(results)
-
-
-def start_app():
-    app.run(port=45678)
 
 
 if __name__ == '__main__':
-    aux = multiprocessing.Process(target=start_app)
-    aux.start()
-
     response = requests.post('http://localhost:12345/connect_agent', json=agent).json()
     print(response)
     agent['token'] = response['data']
+
+    action = {
+        "token": agent['token'],
+        "action": "move",
+        "parameters": [-30.109611, -51.211556]
+    }
 
     response = requests.post('http://localhost:12345/validate_agent', json=agent['token']).json()
     print(response)
@@ -42,7 +23,6 @@ if __name__ == '__main__':
     response = requests.post('http://localhost:12345/job', json=action).json()
     print(response)
 
-    aux.join()
 
 
 
