895b02ed0b9ede08bff07f9b98866bbdf1a63c6b
jojo <joaovbsevero@gmail.com>
2019-04-30 15:26:11 -0300

    Registering if no action was done

--- src/simulation/simulated_enviroment/environment_executors/action_executor.py
+++ src/simulation/simulated_enviroment/environment_executors/action_executor.py
@@ -39,18 +39,19 @@ class ActionExecutor:
             action = (obj['action'], *obj['parameters'])
             result = self.execute(self.world.agents[token], action, cdm_location)
 
-            agent = self.world.agents[obj['token']].__dict__.copy()
+            agent_copy = self.world.agents[obj['token']].__dict__.copy()
             self.logger.register_agent_action(
-                token=agent['token'],
-                role=agent['role'],
+                token=agent_copy['token'],
+                role=agent_copy['role'],
                 result=True if result is None else result,
-                name=agent['agent_info']['name'],
+                name=agent_copy['agent_info']['name'],
                 action=action[0],
                 parameters=action[1]
             )
-            agent_copy = self.world.agents[obj['token']].__dict__.copy()
             del agent_copy['agent_info']
             action_results.append((obj['token'], agent_copy))
+
+        self.logger.register_agent_action()
 
         return action_results
 
