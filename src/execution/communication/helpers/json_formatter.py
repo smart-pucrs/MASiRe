aac7b7ceeb8e8ffb5e1d1c640300606cbaaaed4a
Vinicius Chrisosthemos <vinicius.teixeira.002@acad.pucrs.br>
2019-08-09 12:56:26 -0300

    Remove temporary code

--- src/execution/communication/helpers/json_formatter.py
+++ src/execution/communication/helpers/json_formatter.py
@@ -18,14 +18,11 @@ def simulation_started_format(response, token):
             info['status'] = response['status']
             info['result'] = True
 
-            info['environment']['events'] = format_events(response['event'])
-            info['environment']['step'] = response['step']
-
             found_index = 0
             for idx, actor in enumerate(response['actors']):
                 if actor['token'] == token:
                     if 'role' in actor:
-                        info['agent'] = format_agent(actor)
+                        info['agent'] = actor
                     else:
                         info['social_asset'] = actor
                     found_index = idx
@@ -35,7 +32,7 @@ def simulation_started_format(response, token):
                 return event_error_format('Actor not found in response. ')
 
             response['actors'].pop(found_index)
-            info['event'] = response['event']
+            info['environment'] = response['environment']
 
         info['message'] = response['message']
 
@@ -50,7 +47,7 @@ def simulation_ended_format(response):
 
     :param response: Response from the engine."""
 
-    info = {'status': 0, 'result': False, 'event': {}, 'message': ''}
+    info = {'status': 0, 'result': False, 'environment': {}, 'message': ''}
 
     if response:
         if response['status']:
@@ -80,17 +77,12 @@ def action_results_format(response, token):
             info['status'] = response['status']
             info['result'] = True
 
-            ########## TEMPORARY CODE ###########
-            info['environment']['events'] = format_events(response['event'])
-            info['environment']['step'] = response['step']
-
             found_index = 0
             for idx, actor in enumerate(response['actors']):
                 if 'agent' in actor:
                     if actor['agent']['token'] == token:
-                        info['agent'] = format_agent(actor['agent'])
+                        info['agent'] = actor['agent']
                         info['message'] = actor['message']
-                        print("Result: ", info['agent'])
                         found_index = idx
                         break
                 else:
@@ -104,7 +96,7 @@ def action_results_format(response, token):
                 return event_error_format('Actor not found in response. ')
 
             response['actors'].pop(found_index)
-            info['event'] = response['event']
+            info['environment'] = response['environment']
 
         else:
             info['message'] = response['message']
@@ -115,47 +107,8 @@ def action_results_format(response, token):
         return event_error_format('Empty simulation response. ')
 
 
-########## TEMPORARY CODE ###########
-def format_location(location):
-    return {'lat': location[0], 'lon': location[1]}
-
-
-########## TEMPORARY CODE ###########
-def format_agent(agent_copy):
-    del agent_copy['token']
-    del agent_copy['role']
-    del agent_copy['abilities']
-    del agent_copy['resources']
-    del agent_copy['max_charge']
-    del agent_copy['speed']
-    del agent_copy['size']
-    del agent_copy['physical_capacity']
-    del agent_copy['virtual_capacity']
-    agent_copy['location'] = format_location(agent_copy['location'])
-
-    return agent_copy
-
-
-########## TEMPORARY CODE ###########
-def format_events(old_event):
-    events = []
-
-    if old_event['flood']:
-        events.append(old_event['flood'])
-    events.extend(old_event['victims'])
-    events.extend(old_event['photos'])
-    events.extend(old_event['water_samples'])
-
-    for event in events:
-        event['location'] = format_location(event['location'])
-        if event['type'] == 'photo':
-            del event['victims']
-            del event['analyzed']
-    return events
-
-
 def event_error_format(message):
     """Build a typical error response for the agents. When this function is called, it is very probably that an internal
     error occurred."""
 
-    return {'status': 0, 'result': False, 'event': {}, 'message': f'{message}Possible internal error.'}
+    return {'status': 0, 'result': False, 'environment': {}, 'message': f'{message}Possible internal error.'}
