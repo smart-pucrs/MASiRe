4d17733c75c8e0b8f71daa9d59e7f517c46c9b50
Vinicius Chrisosthemos <vinicius.teixeira.002@acad.pucrs.br>
2019-08-23 15:19:34 -0300

    Create a class to show for the agents the social assets available

---
+++ src/execution/simulation_engine/simulation_objects/social_asset_layer.py
@@ -0,0 +1,6 @@
+class SocialAssetLayer:
+    def __init__(self, identifier, location, profession):
+        self.identifier = identifier
+        self.location = location
+        self.profession = profession
+        self.active = False
