6f2033d88014b6c2e6a72569f3145b3d3f67491a
Vinicius Chrisosthemos <vinicius.teixeira.002@acad.pucrs.br>
2019-10-28 17:40:30 -0300

    Revert "Add size package scripts"
    This reverts commit 2ed5454fa51f62ac25cd1f876edddab419c2fd85.

--- experiments/script_main.sh
+++ experiments/script_main.sh
@@ -3,15 +3,17 @@ SERVER="smart@192.168.1.110"
 DIR_SCRIPTS="Desktop/DisasterSimulator/experiments/temp"
 PASSWORD="Samsung2013"
 
+python3 scripts/local_agents.py &
 sshpass -p $PASSWORD scp -r scripts $SERVER:$DIR_SCRIPTS
 sshpass -p $PASSWORD ssh -tt $SERVER << EOF
-    mkdir $DIR_SCRIPTS/reports
-    python3 $DIR_SCRIPTS/package_size.py
+    mkdir temp
+    python3 $DIR_SCRIPTS/server_agents.py
+    rm -R $DIR_SCRIPTS    
     exit
 EOF
-sshpass -p $PASSWORD scp -r $SERVER:$DIR_SCRIPTS/reports reports
+sshpass -p $PASSWORD scp -r $SERVER:temp reports
 sshpass -p $PASSWORD ssh -tt $SERVER << EOF
-    rm -R $DIR_SCRIPTS
+    rm -R temp
     exit
 EOF
 
--- venv/lib/python3.6/site-packages/osmapi/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import (absolute_import, print_function, unicode_literals)
-
-__version__ = '1.2.2'
-
-from .OsmApi import *  # noqa
